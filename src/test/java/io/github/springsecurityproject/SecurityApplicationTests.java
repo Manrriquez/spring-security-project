57f8a42be001bca0c36c1e8b97074f61436d34fe
Manrriquez <manrriquez.contato@gmail.com>
2023-01-05 23:33:59 -0300

    projeto criado

---
+++ src/test/java/io/github/springsecurityproject/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.springsecurityproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
